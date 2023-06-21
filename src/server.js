7feca65e604b293c77d3b37b33cd384f6ed03e7b
Wisnu Andika <wisnuandika104@gmail.com>
2023-06-21 10:44:38 +0700

    Mendaftarkan Plugin pada Hapi server

--- src/server.js
+++ src/server.js
@@ -1,21 +1,15 @@
+const notesPlugin = require('./notesPlugin');
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes')
 
 const init = async() => {
-    const server = Hapi.server({
-        port: 5000,
-        host: 'localhost',
-        routes: {
-            cors: {
-                origin: ['*'],
-            },
-        },
+    const server = Hapi.server();
+
+    await server.register({
+        plugin: notesPlugin,
+        options: { notes: [] },
     });
 
-    server.route(routes);
-
     await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
 }
 
 init()
