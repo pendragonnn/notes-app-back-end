c4112e76e4bab44c8de9d94bfc049e3cf9e58fe2
Wisnu Andika <wisnuandika104@gmail.com>
2023-06-21 11:37:48 +0700

    Menyiapkan Struktur Proyek

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
-const notesPlugin = require('./notesPlugin');
 const Hapi = require('@hapi/hapi');
-
-const init = async() => {
-    const server = Hapi.server();
-
-    await server.register({
-        plugin: notesPlugin,
-        options: { notes: [] },
-    });
-
-    await server.start();
-}
-
-init()
+const routes = require('./routes');
+ 
+ 
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+ 
+  server.route(routes);
+ 
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+ 
+init();
